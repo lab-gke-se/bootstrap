9b785738c394ef61137e2cff5116c9c7fff9a0af
Dave Butland <dgbutland@gmail.com>
2024-06-05 21:55:14 +0100

    Test wip auth

--- wip.tf
+++ wip.tf
@@ -38,7 +38,7 @@ module "sa_wip_iam_binding_github" {
   service_account_id = module.service_accounts[each.value].id
   role               = module.prj_roles["prj_devops/lab.prj.gke.wifUser"].id
   members = [
-    each.key == "tenant_gke" ? "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/lab-gke-se/bootstrap" : "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/*"
+    each.key == "tenant_gke" ? "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/attribute.repository/lab-gke-se/tenant_gke" : "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github.name}/*"
     # principalSet://iam.googleapis.com/projects/52991355109/locations/global/workloadIdentityPools/github-pool/subject/repo:lab-gke-se/bootstrap:ref:refs/heads/main
     # principalSet://iam.googleapis.com/projects/52991355109/locations/global/workloadIdentityPools/github-pool/subject/repo:lab-gke-se/bootstrap:ref:refs/heads/main
   ]
